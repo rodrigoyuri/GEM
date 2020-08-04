9aa4458bd351aaa7ddb3a3763926c6d400fa07c2
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-08-03 21:31:40 -0300

    Configurações iniciais para as rotas no index.php.

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+require __DIR__."/vendor/autoload.php";
+
+use CoffeeCode\Router\Router;
+
+$router = new Router(URL);
+$router->namespace("Source\Login");
+
+$router->group("ops");
+$router->get("/{$errcode}", function ($data){
+    echo "<h1>Erro {$data["errcode"]}</h1>";
+});
+
+$router->dispatch();
+
+if($router->error()) {
+    $router->redirect("/ops/{$router->error()}");
+}
