e097060e423cd1555ca9ed77fa8a1f4fe3a45324
Evelyn <evelynbrandao15@gmail.com>
2020-08-06 17:27:34 -0300

    Primeira versão do css global e acrescimo de jquery

--- source/login/view/login.php
+++ source/login/view/login.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?= URL_BASE ?>/source/global/css/styles.css">
+    <link rel="stylesheet" href="<?= URL_BASE ?>/source/login/view/css/styles.css">
+
+
     <title>Login</title>
 </head>
+
 <body>
     <h1>Login!</h1>
 
-    <?php if($data): ?>
+    <?php if ($data) : ?>
         <h1>Tem dados</h1>
-        <?php foreach($data as $login): ?>
-            <p><?= $login["nm_login"];?></p>
+        <?php foreach ($data as $login) : ?>
+            <p><?= $login["nm_login"]; ?></p>
         <?php endforeach; ?>
-    <?php else: ?>
+    <?php else : ?>
         <h1>Não tem dados</h1>
-    <?php endif;?>
+    <?php endif; ?>
+
+
+
+
+    <script src="<?= URL_BASE ?>/source/global/js/jquery-3.5.1.min.js"></script>
+    <script src="<?= URL_BASE ?>/source/login/view/js/login.js"></script>
 
 </body>
+
 </html>
