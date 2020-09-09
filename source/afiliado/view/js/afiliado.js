ec56a9b2e93d948a614a9410410df65ebf49138c
Evelyn <evelynbrandao15@gmail.com>
2020-09-09 16:28:35 -0300

    Retorno para antes do editar afiliado v1

--- source/afiliado/view/js/afiliado.js
+++ source/afiliado/view/js/afiliado.js
@@ -225,244 +225,177 @@ $(document).ready(function () {
 			url = form.attr("action");
 		}
 
-		/**
-		 * JS do editar afiliado
-		 */
-		$("#modal-edit-affiliate").on("click", function () {
-			$("#modal-salve-affiliate").show();
-			$(this).hide();
+		const fieldsForm = form.serializeArray();
 
-			$("#form-affiliate :input").attr("disabled", false);
-		})
-
-		$("#modal-salve-affiliate").on("click", function () {
-			$("#modal-edit-affiliate").show();
-			$(this).hide();
-
-			let formArray = $("#form-affiliate").serializeArray()
-
-
-
-			$("#form-affiliate :input").attr("disabled", false);
-
-		})
-
-		/**
-		 * Cliclou no Menu (li a)"Cadastrar Afiliado"
-		 */
-		$("#add-affiliate").on("click", function (e) {
-			e.preventDefault();
-			$("#modal-ver").removeClass("modal-hidden");
-			$(".modal-header button").hide();
-			$(".modal-body .modal-menu").hide();
-
-			$("#form-affiliate button").show();
-
-			$("#form-affiliate :input").attr("disabled", false);
-
-			$(".modal-header #modal-text-type").text("Cadastrar Afiliado")
+		const fields = fieldsForm.filter(function (value) {
+			if (value.name == "cpf") {
+				value.value = removeCharacter(value.value);
+			}
+			if (
+				value.name !== "estado" &&
+				value.name !== "cidade" &&
+				value.name !== "cep" &&
+				value.name !== "bairro"
+			) {
+				return value;
+			}
 		});
 
-		/**
-		 * JS que verifica se o CPF é valido após preencher o input
-		 */
-		$('input[name="cpf"]').focusout(function () {
-			let buttonSend = document.getElementById("enviar");
-			let cpf = removeCharacter($("#cpf").val());
+		const fieldsAddresses = fieldsForm.filter(function (value) {
+			if (
+				value.name === "estado" ||
+				value.name === "cidade" ||
+				value.name === "cep" ||
+				value.name === "bairro"
+			) {
+				return value;
+			}
+		});
 
-			if (validarCPF(cpf)) {
-				buttonSend.disabled = false;
-				$("#enviar").removeClass('button-disable');
-				$("#cpf").css("border-color", "#00E676");
-				$("#cpf").css("background-color", "#00e67765");
+		let addrees = fieldsAddresses.map((e) => e.value).join(";");
+
+		fields.push({ name: "endereco", value: addrees });
+
+		$("#form-affiliate input:checkbox").map(function () {
+			if (this.checked) {
+				return;
 			} else {
-				buttonSend.disabled = true;
-				$("#enviar").addClass('button-disable');
-				$("#cpf").css("border-color", "#E53935");
-				$("#cpf").css("background-color", "#e5383560");
-				alert("O CPF digitado é invalido, por favor digite um CPF valido");
+				fields.push({ name: this.name, value: "" });
 			}
 		});
 
-		/**
-		 * JS que envia os dados para o banco de dados para cadastro
-		 */
-		$("#form-affiliate").submit(function (event) {
-			event.preventDefault();
+		$.ajax({
+			type: type,
+			url: url,
+			data: sortFields(fields),
+			success: function (response) {
+				console.log(response);
+				alert(response);
+				closeModal();
+			},
+			error: function (error) {
+				alert("Erro" + error);
+				console.log("Erro" + error);
+			},
+		});
+	}
 
-			const form = $(this);
+	function removeCharacter(element) {
+		element = element.split("");
+		for (let i = 0; i < element.length; i++) {
+			if (element[i] === "." || element[i] === "-") {
+				element.splice(i, 1);
+			}
+		}
+		return element.join("");
+	}
 
-			const fieldsForm = form.serializeArray();
-
-			const fields = fieldsForm.filter(function (value) {
-				if (value.name == "cpf") {
-					value.value = removeCharacter(value.value);
-				}
-				if (
-					value.name !== "estado" &&
-					value.name !== "cidade" &&
-					value.name !== "cep" &&
-					value.name !== "bairro"
-				) {
-					return value;
-				}
-			});
-
-			const fieldsAddresses = fieldsForm.filter(function (value) {
-				if (
-					value.name === "estado" ||
-					value.name === "cidade" ||
-					value.name === "cep" ||
-					value.name === "bairro"
-				) {
-					return value;
-				}
-			});
-
-			let addrees = fieldsAddresses.map((e) => e.value).join(";");
-
-			fields.push({ name: "endereco", value: addrees });
-
-			$("#form-affiliate input:checkbox").map(function () {
-				if (this.checked) {
-					return;
-				} else {
-					fields.push({ name: this.name, value: "" });
-				}
-			});
-
-			$.ajax({
-				type: "POST",
-				url: form.attr("action"),
-				data: sortFields(fields),
-				success: function (response) {
-					console.log(response);
-					alert(response);
-					closeModal();
-				},
-				error: function (error) {
-					alert("Erro" + error);
-					console.log("Erro" + error);
-				},
-			});
+	function sortFields(array) {
+		array.sort((a, b) => {
+			return a.name > b.name ? 1 : -1;
 		});
 
-		function removeCharacter(element) {
-			element = element.split("");
-			for (let i = 0; i < element.length; i++) {
-				if (element[i] === "." || element[i] === "-") {
-					element.splice(i, 1);
-				}
-			}
-			return element.join("");
-		}
+		return array;
+	}
 
-		function sortFields(array) {
-			array.sort((a, b) => {
-				return a.name > b.name ? 1 : -1;
-			});
+	/**
+	 * JS do Fechar Modal
+	 */
+	$("span[close='modal-ver']").on("click", closeModal);
 
-			return array;
-		}
+	function closeModal() {
+		$("#form-affiliate :input").attr("disabled", false);
+		$(".modal-container").addClass("modal-hidden");
+		$(".modal-body .modal-menu").show();
+		$(".modal-header button").show();
+		$("#form-affiliate")[0].reset();
 
-		/**
-		 * JS do Fechar Modal
-		 */
-		$("span[close='modal-ver']").on("click", closeModal);
+		$(".modal-menu span").removeClass("menu-item-actived");
+		$("span[modal-view=dados-pessoais]").addClass("menu-item-actived");
+		$(".dados section").fadeOut();
+		$("#dados-pessoais").fadeIn();
+	}
 
-		function closeModal() {
-			$("#form-affiliate :input").attr("disabled", false);
-			$(".modal-container").addClass("modal-hidden");
-			$(".modal-body .modal-menu").show();
-			$(".modal-header button").show();
-			$("#form-affiliate")[0].reset();
+	/**
+	 * JS que faz o nav do modal, aparecer e desaparecer div
+	 */
+	$("[modal-view]").on("click", function (param) {
+		param.preventDefault();
+		let span = $(this).attr("modal-view");
 
-			$(".modal-menu span").removeClass("menu-item-actived");
-			$("span[modal-view=dados-pessoais]").addClass("menu-item-actived");
-			$(".dados section").fadeOut();
-			$("#dados-pessoais").fadeIn();
-		}
+		$(".modal-menu span").removeClass("menu-item-actived");
+		// $(this).addClass("menu-item-actived")
+		$("span[modal-view=" + span + "]").addClass("menu-item-actived");
 
-		/**
-		 * JS que faz o nav do modal, aparecer e desaparecer div
-		 */
-		$("[modal-view]").on("click", function (param) {
-			param.preventDefault();
-			let span = $(this).attr("modal-view");
-
-			$(".modal-menu span").removeClass("menu-item-actived");
-			// $(this).addClass("menu-item-actived")
-			$("span[modal-view=" + span + "]").addClass("menu-item-actived");
-
-			/*
+		/*
 				$(".dados section").addClass("modal-hidden")
 				$("#" + span).removeClass("modal-hidden");
 				*/
 
-			// $(".dados section").fadeOut()
-			// $("#" + span).fadeIn();
-			//$(".dados section").hide()
-			//$("#" + span).show();
+		// $(".dados section").fadeOut()
+		// $("#" + span).fadeIn();
+		//$(".dados section").hide()
+		//$("#" + span).show();
 
-			// $("#" + span).fadeIn();
-			$("#" + span).animate({ width: "toggle" }, 500);
-			$(".dados section").animate({ width: "toggle" }).hide();
-			// $("#" + span).show("slide", { direction: "right" }, 500);
-		});
-
-		/**
-		 * JS mostra e oculta div do tipo de afiliado
-		 * Códido do Denis abaixo
-		 */
-		$("#ddlPassport").change(showType);
-
-		function showType() {
-			let s = $("#ddlPassport");
-			if ($(s).val() === "Voluntário") {
-				$("#voluntario").show();
-				$("#assistida").hide();
-			} else if ($(s).val() === "Assistida") {
-				$("#assistida").show();
-				$("#voluntario").hide();
-			} else {
-				$("#voluntario").hide();
-				$("#assistida").hide();
-			}
-		}
-
-		/**
-		 * JS para adição de mascára nos campos necessários
-		 */
-		$('input[name="cpf"]').mask("000.000.000-00");
-		$('input[name="cep"]').mask("00000-000");
-		$('input[name="telefone"]').mask("(00) 0000-0000");
-		$('input[name="celular"]').mask("(00) 00000-0000");
-
-		function validarCPF(input) {
-			let valor = input.split("");
-			let soma = 0;
-			let cont = 10;
-
-			for (let index = 0; index < valor.length - 2; index++) {
-				soma = soma + valor[index] * cont--;
-			}
-
-			let digito1 = 11 - (soma % 11) >= 10 ? 0 : 11 - (soma % 11);
-
-			soma = 0;
-			cont = 11;
-			for (let index = 0; index < valor.length - 1; index++) {
-				soma = soma + valor[index] * cont--;
-			}
-
-			let digito2 = 11 - (soma % 11) >= 10 ? 0 : 11 - (soma % 11);
-
-			if (valor[9] == digito1 && valor[10] == digito2) {
-				console.log("Este CPF é válido");
-				return true;
-			} else {
-				console.log("Este CPF NÃO é válido");
-				return false;
-			}
-		}
+		// $("#" + span).fadeIn();
+		$("#" + span).animate({ width: "toggle" }, 500);
+		$(".dados section").animate({ width: "toggle" }).hide();
+		// $("#" + span).show("slide", { direction: "right" }, 500);
 	});
+
+	/**
+	 * JS mostra e oculta div do tipo de afiliado
+	 * Códido do Denis abaixo
+	 */
+	$("#ddlPassport").change(showType);
+
+	function showType() {
+		let s = $("#ddlPassport");
+		if ($(s).val() === "Voluntário") {
+			$("#voluntario").show();
+			$("#assistida").hide();
+		} else if ($(s).val() === "Assistida") {
+			$("#assistida").show();
+			$("#voluntario").hide();
+		} else {
+			$("#voluntario").hide();
+			$("#assistida").hide();
+		}
+	}
+
+	/**
+	 * JS para adição de mascára nos campos necessários
+	 */
+	$('input[name="cpf"]').mask("000.000.000-00");
+	$('input[name="cep"]').mask("00000-000");
+	$('input[name="telefone"]').mask("(00) 0000-0000");
+	$('input[name="celular"]').mask("(00) 00000-0000");
+
+	function validarCPF(input) {
+		let valor = input.split("");
+		let soma = 0;
+		let cont = 10;
+
+		for (let index = 0; index < valor.length - 2; index++) {
+			soma = soma + valor[index] * cont--;
+		}
+
+		let digito1 = 11 - (soma % 11) >= 10 ? 0 : 11 - (soma % 11);
+
+		soma = 0;
+		cont = 11;
+		for (let index = 0; index < valor.length - 1; index++) {
+			soma = soma + valor[index] * cont--;
+		}
+
+		let digito2 = 11 - (soma % 11) >= 10 ? 0 : 11 - (soma % 11);
+
+		if (valor[9] == digito1 && valor[10] == digito2) {
+			console.log("Este CPF é válido");
+			return true;
+		} else {
+			console.log("Este CPF NÃO é válido");
+			return false;
+		}
+	}
+});
