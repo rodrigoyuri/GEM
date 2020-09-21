31f64817c9e2e54ffc4978cdd09cad03965d24dc
Evelyn <evelynbrandao15@gmail.com>
2020-09-21 12:37:24 -0300

    Submenu de Lista Geral criado e funcionando

--- source/global/components/header.php
+++ source/global/components/header.php
@@ -11,21 +11,23 @@
                 <img src="" alt="">
             </li>
 
-            <li>
+            <li class="nav-home" nav-home>
                 <a href="<?= URL_BASE ?>/admin/lista-geral"><i class="fas fa-clipboard-list"></i>Lista Geral</a>
-            </li>
 
-            <li>
-                <a href="#" id="add-affiliate"><i class="far fa-plus-square"></i>Cadastrar Afiliado</a>
-            </li>
+                <ul class="nav-sub">
+                    <li>
+                        <a href="#" id="add-affiliate"><i class="far fa-plus-square"></i>Cadastrar Afiliado</a>
+                    </li>
 
+                    <li>
+                        <a href="#" id="add-user"><i class="fas fa-user-plus"></i>Cadastrar Usuário</a>
+                    </li>
+                </ul>
+            </li>
             <li>
                 <a href="<?= URL_BASE ?>/admin/lista-chamada"><i class="fas fa-clipboard-check"></i>Chamada</a>
             </li>
 
-            <li>
-                <a href="#" id="add-user"><i class="fas fa-user-plus"></i>Cadastrar Usuário</a>
-            </li>
             <li>
                 <a href="<?= URL_BASE ?>/sair"><i class="fas fa-sign-out-alt"></i>Sair</a>
             </li>
--- source/global/js/global.js
+++ source/global/js/global.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
     $(".button-toggle button").on("click", openToggleMenu)
+    $(".li li").on("click", openSubMenu)
 
-    function openToggleMenu () {
+    function openToggleMenu() {
         $(".nav-bar").toggleClass("visable");
     }
+
+    function openSubMenu() {
+        let path = window.location.pathname;
+
+        if (path.includes("lista-geral")) {
+            $("[nav-home]").addClass("nav-home");
+        } else {
+            $("[nav-home]").removeClass("nav-home");
+        }
+    }
+    openSubMenu()
 });
