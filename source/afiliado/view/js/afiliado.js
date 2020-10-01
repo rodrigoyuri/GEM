860b39c1d1e2b2c4427ac91c75882412bdecfc82
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-10-01 16:33:31 -0300

    input dataNacimento adicionado masck e change type

--- source/afiliado/view/js/afiliado.js
+++ source/afiliado/view/js/afiliado.js
@@ -412,6 +412,7 @@ $(document).ready(function () {
 	$('input[name="cep"]').mask("00000-000");
 	$('input[name="telefone"]').mask("(00) 0000-0000");
 	$('input[name="celular"]').mask("(00) 00000-0000");
+	$('input[name="dataNascimento"]').mask("'00/00/0000'");
 
 	function validarCPF(input) {
 		let valor = input.split("");
