1c1252d8fe289b8e127939100fe2770a1be5e2c1
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-27 13:42:28 -0300

    Mudança no layout de cadastro e update

--- source/afiliado/view/lista-geral.php
+++ source/afiliado/view/lista-geral.php
@@ -63,12 +63,12 @@
                 <span close="modal-ver"><i class="fas fa-times"></i></span>
             </div>
             <div class="modal-body">
-                <nav class="modal-menu">
+                <!-- <nav class="modal-menu">
                     <span class="modal-menu-item menu-item-actived" modal-view="dados-pessoais">Dados Pessoais</span>
                     <span class="modal-menu-item" modal-view="dados-contatos">Dados Contato</span>
                     <span class="modal-menu-item" modal-view="dados-afiliados">Dados Afiliado</span>
                     <span class="modal-menu-item" modal-view="dados-cedidos">Beneficios cedidos</span>
-                </nav>
+                </nav> -->
                 <div class="dados form-outer">
                     <input type="text" name="codAfiliado" id="codAfiliado" hidden>
                     <form id="form-affiliate" action="<?= URL_BASE ?>/admin/cadastro-afiliado" method="POST">
@@ -94,11 +94,6 @@
                                 <div class="label">Data de nascimento</div>
                                 <input type="text" class="input-base" name="data" id="data" title="Data de Nascimento">
                             </div>
-                            <div class="field">
-                                <button class="firstNext next" modal-view="dados-contatos">Próximo</button>
-                            </div>
-                        </section>
-                        <section id="dados-contatos" class="page modal-hidden">
                             <div class="title">Informações de contato:</div>
                             <div class="field">
                                 <div class="label">Estado</div>
@@ -210,7 +205,7 @@
                                 <div class="label">Complemento</div>
                                 <input type="text" class="input-base" name="complemento" id="complemento" title="Complemento">
                             </div>
-                            
+
                             <div class="field">
                                 <div class="label">CEP</div>
                                 <input type="text" class="input-base" name="cep" id="cep" title="CEP">
@@ -227,12 +222,6 @@
                                 <div class="label">Email</div>
                                 <input type="email" class="input-base" name="email" id="email" placeholder="exemplo@gmail.com" title="E-mail">
                             </div>
-                            <div class="field btns">
-                                <button class="prev-1 prev" modal-view="dados-pessoais">Anterior</button>
-                                <button class="next-1 next" modal-view="dados-afiliados">Próximo</button>
-                            </div>
-                        </section>
-                        <section id="dados-afiliados" class="page modal-hidden">
                             <div class="title">Outras informações:</div>
                             <div class="field">
                                 <div class="label">Sexo</div>
@@ -284,19 +273,218 @@
                                     </div>
                                 </div>
                             </div>
-                            
+
                             <div id="assistida">
                                 <div class="field">
                                     <div class="label">Estado da Assistida</div>
                                     <input type="text" class="input-base" name="estado_da_assistida" id="estado_da_assistida" placeholder="" title="Estado da Assistida">
                                 </div>
                             </div>
-                            <div class="field btns">
-                                <button class="prev-2 prev" modal-view="dados-contatos">Anterior</button>
+                            <div class="field">
                                 <button class="submit" id="enviar">Enviar</button>
                             </div>
                         </section>
                     </form>
+                    </section>
+                    <!-- <section id="dados-contatos" class="page modal-hidden">
+                        <div class="title">Informações de contato:</div>
+                        <div class="field">
+                            <div class="label">Estado</div>
+                            <select class="input-base" name="estado" id="estado" title="Estado">
+                                <option class="" value="Estado">
+                                    Estado
+                                </option>
+                                <option class="" value="AC">
+                                    Acre
+                                </option>
+                                <option class="" value="AL">
+                                    Alagoas
+                                </option>
+                                <option class="" value="AP">
+                                    Amapá
+                                </option>
+                                <option class="" value="AM">
+                                    Amazonas
+                                </option>
+                                <option class="" value="BA">
+                                    Bahia
+                                </option>
+                                <option class="" value="CE">
+                                    Ceará
+                                </option>
+                                <option class="" value="DF">
+                                    Distrito Federal
+                                </option>
+                                <option class="" value="ES">
+                                    Espírito Santo
+                                </option>
+                                <option class="" value="GO">
+                                    Goiás
+                                </option>
+                                <option class="" value="MA">
+                                    Maranhão
+                                </option>
+                                <option class="" value="MT">
+                                    Mato Grosso
+                                </option>
+                                <option class="" value="MS">
+                                    Mato Grosso do Sul
+                                </option>
+                                <option class="" value="MG">
+                                    Minas Gerais
+                                </option>
+                                <option class="" value="PA">
+                                    Pará
+                                </option>
+                                <option class="" value="PB">
+                                    Paraíba
+                                </option>
+                                <option class="" value="PR">
+                                    Paraná
+                                </option>
+                                <option class="" value="PE">
+                                    Pernambuco
+                                </option>
+                                <option class="" value="PI">
+                                    Piauí
+                                </option>
+                                <option class="" value="RJ">
+                                    Rio de Janeiro
+                                </option>
+                                <option class="" value="RN">
+                                    Rio Grande do Norte
+                                </option>
+                                <option class="" value="RS">
+                                    Rio Grande do Sul
+                                </option>
+                                <option class="" value="RO">
+                                    Rondônia
+                                </option>
+                                <option class="" value="RR">
+                                    Roraima
+                                </option>
+                                <option class="" value="SC">
+                                    Santa Catarina
+                                </option>
+                                <option class="" value="SP">
+                                    São Paulo
+                                </option>
+                                <option class="" value="SE">
+                                    Sergipe
+                                </option>
+                                <option class="" value="TO">
+                                    Tocantins
+                                </option>
+                            </select>
+                        </div>
+                        <div class="field">
+                            <div class="label">Cidade</div>
+                            <input type="text" class="input-base" name="cidade" id="cidade" title="Cidade">
+                        </div>
+                        <div class="field">
+                            <div class="label">Bairro</div>
+                            <input type="text" class="input-base" name="bairro" id="bairro" title="Bairro">
+                        </div>
+
+                        <div class="field">
+                            <div class="label">Rua</div>
+                            <input type="text" class="input-base" name="rua" id="rua" title="Rua">
+                        </div>
+                        <div class="field">
+                            <div class="label">Nº</div>
+                            <input type="number" class="input-base" name="numero" id="numero" title="Numero">
+                        </div>
+                        <div class="field">
+                            <div class="label">Complemento</div>
+                            <input type="text" class="input-base" name="complemento" id="complemento" title="Complemento">
+                        </div>
+
+                        <div class="field">
+                            <div class="label">CEP</div>
+                            <input type="text" class="input-base" name="cep" id="cep" title="CEP">
+                        </div>
+                        <div class="field">
+                            <div class="label">Telefone</div>
+                            <input type="text" class="input-base" name="telefone" id="telefone" placeholder="(00)0000-0000" title="Telefone Princial">
+                        </div>
+                        <div class="field">
+                            <div class="label">Celular</div>
+                            <input type="text" class="input-base" name="celular" id="celular" placeholder="(00)00000-0000" title="Telefone para contato">
+                        </div>
+                        <div class="field">
+                            <div class="label">Email</div>
+                            <input type="email" class="input-base" name="email" id="email" placeholder="exemplo@gmail.com" title="E-mail">
+                        </div>
+                        <div class="field btns">
+                            <button class="prev-1 prev" modal-view="dados-pessoais">Anterior</button>
+                            <button class="next-1 next" modal-view="dados-afiliados">Próximo</button>
+                        </div>
+                    </section> -->
+                    <!-- <section id="dados-afiliados" class="page modal-hidden">
+                        <div class="title">Outras informações:</div>
+                        <div class="field">
+                            <div class="label">Sexo</div>
+                            <select name="sexo" class="input-base">
+                                <option value="Feminino">Feminino</option>
+                                <option value="Masculino">Masculino</option>
+                            </select>
+                        </div>
+                        <div class="field">
+                            <div class="label">Data de Ingressão</div>
+                            <input type="text" class="input-base" name="data_ingressao" id="data_ingressao" title="Data de Ingressão">
+                        </div>
+                        <div class="field">
+                            <div class="label">Tipo</div>
+                            <select id="ddlPassport" name="tipo" class="input-base">
+                                <option value="Assistida">Assistida</option>
+                                <option value="Voluntário">Voluntário</option>
+                            </select>
+                        </div>
+                        <div id="voluntario">
+                            <div class="field">
+                                <div class="label">Area de interesse</div>
+                                <input type="text" name="funcao" class="input-base">
+                            </div>
+                            <div class="field">
+                                <div class="label">Qualificação Profissional</div>
+                                <input type="text" name="qualificacao" class="input-base">
+                            </div>
+
+                            <div class="field">
+                                <div class="label">Disponibilidade</div>
+                                <div class="todas">
+                                    <div class="div">
+                                        <input type="checkbox" name="week[]" value="segunda">
+                                        <label for="">Segunda</label>
+                                    </div>
+                                    <div class="div">
+                                        <input type="checkbox" name="week[]" value="terça">
+                                        <label for="">Terça</label>
+                                    </div>
+                                    <div class="div">
+                                        <input type="checkbox" name="week[]" value="quarta">
+                                        <label for="">Quarta</label>
+                                    </div>
+                                    <div class="div">
+                                        <input type="checkbox" name="week[]" value="quinta">
+                                        <label for="">Quinta</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="assistida">
+                            <div class="field">
+                                <div class="label">Estado da Assistida</div>
+                                <input type="text" class="input-base" name="estado_da_assistida" id="estado_da_assistida" placeholder="" title="Estado da Assistida">
+                            </div>
+                        </div>
+                        <div class="field btns">
+                            <button class="prev-2 prev" modal-view="dados-contatos">Anterior</button>
+                            <button class="submit" id="enviar">Enviar</button>
+                        </div>
+                    </section>
+                    </form> -->
                     <section id="dados-cedidos" class="page modal-hidden">
                         <form action="<?= URL_BASE ?>/admin/cadastro-item" id="form-items">
                             <input type="number" name="qtItem" id="" class="input-base input-item-num">
